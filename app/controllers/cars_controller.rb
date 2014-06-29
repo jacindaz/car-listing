836cccd21e20c876b71facc1ccef3eee34ee032b
Jacinda Zhong <jacindaz@gmail.com>
2014-06-29 14:59:31 -0400

    Updates cars controller, adds show page view

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,16 +2,28 @@ class CarsController < ApplicationController
 
   def index
     @cars = Car.all
-    @title = "Welcome to the Awesomest Cars Listing Site Ever"
+    @title = "So many Cars"
   end
 
   def show
+    @car = Car.find(params[:id])
+    @title = "About this Fabulous Car"
   end
 
   def new
+    @car = Car.new
   end
 
   def create
+    @car = Car.find(params[:id])
+
+    if @car.save
+      flash[:notice] = "Car much submitted."
+      redirect_to car_path(@car)
+    else
+      flash[:notice] = "Much sorry, car very unsaveable."
+      render :'cars/new'
+    end
   end
 
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ActiveRecord::Base
   validates :manufacturer_id, presence: true, uniqueness: { scope: :mileage}
   validates :color, presence: true
-  validates :year, presence: true, inclusion: { within: 1920..2014 }
+  validates :year, presence: true, length: { is: 4 }
   validates :mileage, presence: true
 
   belongs_to :manufacturer
