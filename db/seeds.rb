43b18e29ef9a79df6a84c567140a4ce43a57b58b
Jacinda Zhong <jacindaz@gmail.com>
2014-07-01 09:44:24 -0400

    Updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Manufacturer.create(name: "Toyota", country: "Japan")
-# Manufacturer.create(name: "Honda", country: "Japan")
-# Manufacturer.create(name: "Ferrari", country: "Italy")
-# Manufacturer.create(name: "Volkswagen", country: "Germany")
+Manufacturer.create(name: "Toyota", country: "Japan")
+Manufacturer.create(name: "Honda", country: "Japan")
+Manufacturer.create(name: "Ferrari", country: "Italy")
+Manufacturer.create(name: "Volkswagen", country: "Germany")
 
 
 Car.create!(manufacturer_id: 2, color: "red", year: "2014",
             mileage: 100, description: "Awesome red prius")
 Car.create!(manufacturer_id: 4, color: "green", year: "2012",
             mileage: 5, description: "The cutest little green beetle.")
+
+
